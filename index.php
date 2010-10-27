3792ff4b909463a51a26dc55b279ba7ed2ea992b
Arkadiusz Neuman <mrmoro@o2.pl>
2010-10-27 15:22:54 +0200

    poczatki bazy
    Signed-off-by: Arkadiusz Neuman <mrmoro@o2.pl>

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
     include('sql.php');
-    $database = new sql();
+    $sql = new Sql("127.0.0.1", "root", "", "database");
+    //$sql->AddNews("Test", "testestestes");
+    $sql->ReadNews();
+    $sql->Close();
 
 ?>
--- sql.php
+++ sql.php
@@ -1,9 +1,45 @@
 <?php
     class sql
     {
-        function sql()
+        function Sql($server, $user, $pass, $database) //polaczenie z wybraniem bazy
         {
-            echo "Łączenie z bazą";
+            $this->sql_conn = mysql_connect($server, $user, $pass);
+            mysql_select_db($database);
+        }
+
+        function SelectDatabase($database) //zmiana bazy w tym miejscu
+        {
+            mysql_select_db($database);
+        }
+        
+        function AddNews($title, $note)
+        {
+            $query = "INSERT INTO `news` (`id`, `title`, `date`, `note`) VALUES
+            (NULL, '".$title."', ' ".date("Y-m-d H:i:s")."', '".$note."');";
+
+            if (mysql_query($query))
+                echo "wysłane";
+            else
+                echo "nie wysłane";
+        }
+
+        function ReadNews()
+        {
+            $query = "SELECT title, date, note FROM news ORDER BY date DESC";
+            $reply = mysql_query($query);
+
+            while ($line = mysql_fetch_row($reply)) //do czegos by bylo trzeba to ciulstwo zapisac i zwrocic w funkcji te dane, tylko chuj wie do czego. tworzyc nowa klase news?
+            {
+                echo "<h3>$line[0]</h3><h6><br />"; //TODO tylko zapis do czegos, bez wyswietlenia
+                echo "$line[1]</h6><br />";
+                echo "<hr />";
+                echo "<p>".nl2br($line[2])."</p><br>";
+            }
+        }
+
+        function Close()
+        {
+            mysql_close($this->sql_conn);
         }
     }
 ?>

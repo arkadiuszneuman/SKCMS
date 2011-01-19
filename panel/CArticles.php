f9cb4f4ac3b708f2abcc3eabec61a8d181fa8327
Maciej Płaza <plaza.maciej@gmail.com>
2011-01-19 23:23:40 +0100

    Better security
    Signed-off-by: Maciej Płaza <plaza.maciej@gmail.com>

--- panel/CArticles.php
+++ panel/CArticles.php
@@ -9,82 +9,89 @@ class CArticles extends CPanel
 
     public function Articles()
     {
-        //przycisk, ktorym przenosi sie do kosza
-        if (isset($_POST['moveToBin']))
-        {
-            @$page = $_GET['page'];
-            if ($page == null)
-                $page = 0;
+		if (Privileges::CheckPrivilege(Privileges::ARTICLES, $this->privileges))
+		{
+			//przycisk, ktorym przenosi sie do kosza
+			if (isset($_POST['moveToBin']))
+			{
+				@$page = $_GET['page'];
+				if ($page == null)
+					$page = 0;
 
-            $isChanged = false; //czy jakis zostal zmodyfikowany
-            if ($this->sql->UpdateArticleLink($_POST['visibleIn'], $page, Sql::NOTHING, $isChanged))
-            {
-                if ($isChanged)
-                    $this->SendInfo("Link/Linki do newsów zostały zaktualizowane");
-            }
-            else
-                $this->SendInfo("Link/Linki do newsów nie zostały zaktualizowane");
+				$isChanged = false; //czy jakis zostal zmodyfikowany
+				if ($this->sql->UpdateArticleLink($_POST['visibleIn'], $page, Sql::NOTHING, $isChanged))
+				{
+					if ($isChanged)
+						$this->SendInfo("Link/Linki do newsów zostały zaktualizowane");
+				}
+				else
+					$this->SendInfo("Link/Linki do newsów nie zostały zaktualizowane");
 
-            @$checkboxes = $_POST['check']; //zlapanie z formularza checknietych checkboxow
+				@$checkboxes = $_POST['check']; //zlapanie z formularza checknietych checkboxow
 
-            if (count($checkboxes) > 0)
-            {
-                if ($this->sql->ArticlesToBin($checkboxes))
-                    $this->SendInfo("Artykuł/Artykuły zostały przeniesione do kosza");
-                else
-                    $this->SendInfo("Nie można przenieść artykułu/artykułów do kosza");
-            }
+				if (count($checkboxes) > 0)
+				{
+					if ($this->sql->ArticlesToBin($checkboxes))
+						$this->SendInfo("Artykuł/Artykuły zostały przeniesione do kosza");
+					else
+						$this->SendInfo("Nie można przenieść artykułu/artykułów do kosza");
+				}
 
-            $id = 0; //zeby przeszedl do malowania tabelki
-        }
-        else if(isset($_POST['submit'])) //po kliknieciu wyslij przy edycji notki
-        {
-            $title = $_POST['title'];
-            $note = $_POST['note'];
-            @$id = $_GET['id'];
-			$author = $_POST['author'];
-			$link = $_POST['link'];
+				$id = 0; //zeby przeszedl do malowania tabelki
+			}
+			else if(isset($_POST['submit'])) //po kliknieciu wyslij przy edycji notki
+			{
+				$title = $_POST['title'];
+				$note = $_POST['note'];
+				@$id = $_GET['id'];
+				$author = $_POST['author'];
+				$link = $_POST['link'];
 
-            $title = trim($title);
-            $note = trim($note);
+				$title = trim($title);
+				$note = trim($note);
 
-            if (empty($title) || empty($note))
-                $this->SendInfo("News nie zaktualizowany z powodu braku tytułu lub treści");
-            else if ($id < 1)
-                $this->SendInfo("News nie zaktualizowany z powodu złego id");
-            else
-            {
-                if ($this->sql->EditArticle($id, $title, $note, $author, $link))
-                    $this->SendInfo("News został zaktualizowany");
-                else
-                    $this->SendInfo("News nie został zaktualizowany");
+				if (empty($title) || empty($note))
+					$this->SendInfo("News nie zaktualizowany z powodu braku tytułu lub treści");
+				else if ($id < 1)
+					$this->SendInfo("News nie zaktualizowany z powodu złego id");
+				else
+				{
+					if ($this->sql->EditArticle($id, $title, $note, $author, $link))
+						$this->SendInfo("News został zaktualizowany");
+					else
+						$this->SendInfo("News nie został zaktualizowany");
 
-                $id = 0; //zeby przeszedl do malowania tabelki
-            }
+					$id = 0; //zeby przeszedl do malowania tabelki
+				}
 
-            ?><br /><?php
-        }
-        else
-        {
-            @$id = $_GET['id'];
-        }
+				?><br /><?php
+			}
+			else
+			{
+				@$id = $_GET['id'];
+			}
 
-        if ($id != 0) //jesli wybrana zostala jakas notka to dawaj formularz, a jesli nie...
-        {
-            $news = $this->sql->ReadArticle($id);
-            $this->DrawTextAreas("articles&id=$id", $news['title'], $news['note'], $news['author'], $news['link']);
-        }
-        else //... to wyswietli sie lista notek do wybrania
-        {
-            $this->DrawInfo();
+			if ($id != 0) //jesli wybrana zostala jakas notka to dawaj formularz, a jesli nie...
+			{
+				$news = $this->sql->ReadArticle($id);
+				$this->DrawTextAreas("articles&id=$id", $news['title'], $news['note'], $news['author'], $news['link']);
+			}
+			else //... to wyswietli sie lista notek do wybrania
+			{
+				$this->DrawInfo();
 
-            @$page = $_GET['page'];
+				@$page = $_GET['page'];
 
-            $news = $this->sql->ReadArticles(Sql::NOTHING, $page*$this->howMany, $this->howMany);
-            $count = $this->sql->NumberOfArticles(Sql::NOTHING);
-            $this->DrawTable($news, CPanel::EDIT, $this->sql->ReadLinks());
-            $this->DrawPaging($count);
-        }
+				$news = $this->sql->ReadArticles(Sql::NOTHING, $page*$this->howMany, $this->howMany);
+				$count = $this->sql->NumberOfArticles(Sql::NOTHING);
+				$this->DrawTable($news, CPanel::EDIT, $this->sql->ReadLinks());
+				$this->DrawPaging($count);
+			}
+		}
+		else
+		{
+			echo "Nie możesz tu być!";
+		}
     }
 }
 
--- panel/CLinks.php
+++ panel/CLinks.php
@@ -9,88 +9,95 @@ class CLinks extends CPanel
 
     public function Links()
     {
-        @$id = $_GET['id'];
-        if ($id == null)
-            $id = 0;
+		if (Privileges::CheckPrivilege(Privileges::MENU, $this->privileges))
+		{
+			@$id = $_GET['id'];
+			if ($id == null)
+				$id = 0;
 
-        if (isset($_POST['remove']))
-        {
-            @$checkboxes = $_POST['check']; //zlapanie z formularza checknietych checkboxow
-            if (count($checkboxes) > 0)
-            {
-                if ($this->sql->RemoveLink($checkboxes))
-                    $this->SendInfo("Link/linki zostały usunięte");
-                else
-                    $this->SendInfo("Nie można usunąć linku/linków");
-            }
-            else
-                 $this->SendInfo("Nie zaznaczono żadnego linku");
-        }
-        else if(isset($_POST['newlink'])) //dodanie nowego linku
-        {
-            $link = $_POST['link'];
-            $link = trim($link);
+			if (isset($_POST['remove']))
+			{
+				@$checkboxes = $_POST['check']; //zlapanie z formularza checknietych checkboxow
+				if (count($checkboxes) > 0)
+				{
+					if ($this->sql->RemoveLink($checkboxes))
+						$this->SendInfo("Link/linki zostały usunięte");
+					else
+						$this->SendInfo("Nie można usunąć linku/linków");
+				}
+				else
+					$this->SendInfo("Nie zaznaczono żadnego linku");
+			}
+			else if(isset($_POST['newlink'])) //dodanie nowego linku
+			{
+				$link = $_POST['link'];
+				$link = trim($link);
 
-            if (empty($link))
-            {
-                $this->SendInfo("Nie dodano nowego linku z powodu braku nazwy linku");
-            }
-            else
-            {
-                if ($this->sql->AddLink($link))
-                    $this->SendInfo("Link został dodany");
-                else
-                    $this->SendInfo("Link nie został dodany");
-            }
+				if (empty($link))
+				{
+					$this->SendInfo("Nie dodano nowego linku z powodu braku nazwy linku");
+				}
+				else
+				{
+					if ($this->sql->AddLink($link))
+						$this->SendInfo("Link został dodany");
+					else
+						$this->SendInfo("Link nie został dodany");
+				}
 
-            ?><br /><?php
-        }
-        else if (isset($_POST['editlink'])) //lub edycja linku
-        {
-            $link = $_POST['link'];
-            $link = trim($link);
+				?><br /><?php
+			}
+			else if (isset($_POST['editlink'])) //lub edycja linku
+			{
+				$link = $_POST['link'];
+				$link = trim($link);
 
-            if (empty($link))
-            {
-                $this->SendInfo("Nie dodano nowego linku z powodu braku nazwy linku");
-            }
-            else
-            {
-                if ($this->sql->EditLink($id, $link))
-                    $this->SendInfo("Link został zmieniony");
-                else
-                    $this->SendInfo("Link nie został zmieniony");
-            }
+				if (empty($link))
+				{
+					$this->SendInfo("Nie dodano nowego linku z powodu braku nazwy linku");
+				}
+				else
+				{
+					if ($this->sql->EditLink($id, $link))
+						$this->SendInfo("Link został zmieniony");
+					else
+						$this->SendInfo("Link nie został zmieniony");
+				}
 
-            ?><br /><?php
-        }
-        else if (isset($_GET['do']) && $_GET['do'] == "saveOrder")
-        {
-            if ($this->sql->SaveOrder())
-                $this->SendInfo("Kolejność linków została zmieniona");
-            else
-                $this->SendInfo("Kolejność linków nie została zmieniona (czy zostały wprowadzone jakiekolwiek zmiany?)");
-        }
+				?><br /><?php
+			}
+			else if (isset($_GET['do']) && $_GET['do'] == "saveOrder")
+			{
+				if ($this->sql->SaveOrder())
+					$this->SendInfo("Kolejność linków została zmieniona");
+				else
+					$this->SendInfo("Kolejność linków nie została zmieniona (czy zostały wprowadzone jakiekolwiek zmiany?)");
+			}
 
-        $this->DrawInfo();
+			$this->DrawInfo();
 
-        $przenies = "?task=links";
-        if ($id != 0)
-            $przenies = $przenies."&id=$id";
-        $form = new CForm(CForm::POST, $przenies);
-        $text = new CTextBox("Dodaj nowy link: ", "link");
-        if ($id != 0)
-        {
-            $link = $this->sql->ReadLinks((int)$id);
-            $text->SetValue($link[0]['link']);
-        }
-        $text->SetAddionalAttribs('size="65"');
-        $form->AddItem($text);
-        $form->AddItem(new CButton((($id != 0) ? "Edytuj" : "Wyślij"), (($id != 0) ? "editlink" : "newlink")));
-        $form->Draw();
+			$przenies = "?task=links";
+			if ($id != 0)
+				$przenies = $przenies."&id=$id";
+			$form = new CForm(CForm::POST, $przenies);
+			$text = new CTextBox("Dodaj nowy link: ", "link");
+			if ($id != 0)
+			{
+				$link = $this->sql->ReadLinks((int)$id);
+				$text->SetValue($link[0]['link']);
+			}
+			$text->SetAddionalAttribs('size="65"');
+			$form->AddItem($text);
+			$form->AddItem(new CButton((($id != 0) ? "Edytuj" : "Wyślij"), (($id != 0) ? "editlink" : "newlink")));
+			$form->Draw();
 
-        $links = $this->sql->ReadLinks();
-        $this->DrawTable($links, CPanel::LINKS);
+			$links = $this->sql->ReadLinks();
+			$this->DrawTable($links, CPanel::LINKS);
+		}
+		else
+		{
+			echo "Nie możesz tu być!";
+		}
     }
 }
 

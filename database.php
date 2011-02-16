55844fc37d55044f89b68a19f209d8be7546fd50
Maciej Płaza <plaza.maciej@gmail.com>
2011-02-16 16:09:37 +0100

    Temporary fix
    Signed-off-by: Maciej Płaza <plaza.maciej@gmail.com>

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $SQLdatabase = "database";
-	$SQLdatabasePrefix = "skcms_";
-    $SQLlogin = "root";
-    $SQLpass = "";
-    $SQLserver = "127.0.0.1";
-
-?>
---
+++ includes/database.php
@@ -0,0 +1,9 @@
+<?php
+
+    $SQLdatabase = "skcms";
+    $SQLdatabasePrefix = "skcms_";
+    $SQLlogin = "root";
+    $SQLpass = "maciekp";
+    $SQLserver = "localhost";
+
+?>
